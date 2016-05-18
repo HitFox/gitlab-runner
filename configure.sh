b2a134a07c34d4f2e18b80f72c1366684b63be80
Martin Held <dleh.nitram@gmail.com>
2016-05-18 16:45:00 +0200

    Adds privileged flag

--- configure.sh
+++ configure.sh
@@ -1,4 +1,4 @@
-gitlab-runner register -n -r $GITLAB_RUNNER_TOKEN -u $GITLAB_CI_URL --executor docker --docker-image $GITLAB_RUNNER_IMAGE --description ${GITLAB_RUNNER_DESC}_1
-gitlab-runner register -n -r $GITLAB_RUNNER_TOKEN -u $GITLAB_CI_URL --executor docker --docker-image $GITLAB_RUNNER_IMAGE --description ${GITLAB_RUNNER_DESC}_2
+gitlab-runner register -n -r $GITLAB_RUNNER_TOKEN -u $GITLAB_CI_URL --executor docker --docker-image $GITLAB_RUNNER_IMAGE --description ${GITLAB_RUNNER_DESC}_1 --docker-privileged
+gitlab-runner register -n -r $GITLAB_RUNNER_TOKEN -u $GITLAB_CI_URL --executor docker --docker-image $GITLAB_RUNNER_IMAGE --description ${GITLAB_RUNNER_DESC}_2 --docker-privileged
 
 /usr/bin/gitlab-runner run --user=gitlab-runner --working-directory=/home/gitlab-runner
